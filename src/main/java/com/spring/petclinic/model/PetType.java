6c4c975d673a6a363cd5084cdc26061c361bf5a8
Laurel Lao Ang <laurellaoang@gmail.com>
2021-08-27 09:57:23 +0800

    POJO model created
    Closes #2

---
+++ src/main/java/com/spring/petclinic/model/PetType.java
@@ -0,0 +1,13 @@
+package com.spring.petclinic.model;
+
+public class PetType extends Pet{
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
