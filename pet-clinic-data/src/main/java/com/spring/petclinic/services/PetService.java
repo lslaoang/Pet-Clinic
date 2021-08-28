1af5eddb42a748b5e62e2a98e2736cc10e2e0b03
Laurel Lao Ang <laurellaoang@gmail.com>
2021-08-28 10:11:38 +0800

    Added Services Interface. Closes #5

---
+++ pet-clinic-data/src/main/java/com/spring/petclinic/services/PetService.java
@@ -0,0 +1,13 @@
+package com.spring.petclinic.services;
+
+import com.spring.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
