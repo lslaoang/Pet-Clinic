91f7908117fa26ec72aa3c088325457c62f7719e
Laurel Lao Ang <laurellaoang@gmail.com>
2021-10-10 19:01:26 +0800

    Added Pets and Contact info to owner on start up. ID validation created. Closes #33

--- pet-clinic-web/src/main/java/com/spring/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/spring/petclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.spring.petclinic.bootstrap;
 
 import com.spring.petclinic.model.Owner;
+import com.spring.petclinic.model.Pet;
 import com.spring.petclinic.model.PetType;
 import com.spring.petclinic.model.Vet;
 import com.spring.petclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import com.spring.petclinic.services.PetTypeService;
 import com.spring.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -41,12 +44,26 @@ public class DataLoader implements CommandLineRunner {
         owner1.setTelephone("123123123");
         ownerService.save(owner1);
 
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDogPetType);
+        mikesPet.setOwner(owner1);
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setName("Rosco");
+        owner1.getPets().add(mikesPet);
+
         Owner owner2 = new Owner();
         owner2.setFirstName("Zoe");
         owner2.setLastName("Binarao");
         owner2.setAddress("166 Cembo");
         owner2.setCity("Makati");
         owner2.setTelephone("2323232323");
+
+        Pet fionasCat = new Pet();
+        fionasCat.setName("Just Cat");
+        fionasCat.setOwner(owner2);
+        fionasCat.setBirthDate(LocalDate.now());
+        fionasCat.setPetType(savedCatPetType);
+        owner2.getPets().add(fionasCat);
 
 
         ownerService.save(owner2);
