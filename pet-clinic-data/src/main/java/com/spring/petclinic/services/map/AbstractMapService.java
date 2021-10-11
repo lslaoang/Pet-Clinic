c721b8f39a1f2cdbd77773ee6e017eb009708549
Laurel Lao Ang <laurellaoang@gmail.com>
2021-10-11 10:03:32 +0800

    Notes added

--- pet-clinic-data/src/main/java/com/spring/petclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/spring/petclinic/services/map/AbstractMapService.java
@@ -7,6 +7,10 @@ import java.util.*;
 public abstract class AbstractMapService<T extends BaseEntity,ID extends Long> {
     protected Map<Long,T> map = new HashMap();
 
+    /*
+    Setting generics for services.
+    */
+
     Set<T> findAll(){
         return new HashSet<>(map.values());
     }
@@ -16,6 +20,7 @@ public abstract class AbstractMapService<T extends BaseEntity,ID extends Long> {
     }
 
     T save(T object){
+        //Object validation before saving
         if(object !=null){
             if(object.getId()==null){
                 object.setId(getNextId());
