df7cec21e0d9af86aeb2ce344d9985675edca4ec
Laurel Lao Ang <laurellaoang@gmail.com>
2021-10-14 16:42:09 +0800

    Created PetSDJpaService JPA entity Closes #47

---
+++ pet-clinic-data/src/main/java/com/spring/petclinic/services/springdatajpa/PetSDAJpaService.java
@@ -0,0 +1,48 @@
+package com.spring.petclinic.services.springdatajpa;
+
+import com.spring.petclinic.model.Pet;
+import com.spring.petclinic.repositories.PetRepository;
+import com.spring.petclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetSDAJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetSDAJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> petSet = new HashSet<>();
+        petRepository.findAll().forEach(petSet::add);
+        return petSet;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
