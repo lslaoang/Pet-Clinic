c778cb8344f6aeaf1d675916ad37def445b80db2
Laurel Lao Ang <laurellaoang@gmail.com>
2021-10-14 17:52:44 +0800

    JPA service created for Visit Closes #49

---
+++ pet-clinic-data/src/main/java/com/spring/petclinic/services/springdatajpa/VisitSDJpaService.java
@@ -0,0 +1,48 @@
+package com.spring.petclinic.services.springdatajpa;
+
+import com.spring.petclinic.model.Visit;
+import com.spring.petclinic.repositories.VisitRepository;
+import com.spring.petclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VisitSDJpaService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitSDJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visitSet = new HashSet<>();
+        visitRepository.findAll().forEach(visitSet::add);
+        return visitSet;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        return visitRepository.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        visitRepository.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
